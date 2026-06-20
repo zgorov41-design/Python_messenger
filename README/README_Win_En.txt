f362b2b3ce2bb365c4255c29085565e557b13d19
zgorov41-disign <zgorov41@gmail.com>
2026-06-20 14:51:38 +0300

    Первый коммит: базовый функционал мессенджера

---
+++ README/README_Win_En.txt
@@ -0,0 +1,34 @@
+This messenger only works on one Wi-Fi network or a wired connection.
+If a person is connected to another network, you will not be able to contact them.
+Unpack it first.zip archive to any location
+Also, for the messenger to work, a piotn version of at least 3.12.3
+is required to run on Windows.:Open Cmd(Win+R, type cmd) and run
+the following commands (install packages):
+install venv(virtual environment):
+ Go to the directory
+    cd (path to the messager folder)
+ Install venv yourself:
+    python3 -m venv venv
+ Run venv (also in cmd)
+   venv\Scripts\activate
+ After activation, the prefix (venv):
+(venv) "User name" will appear at the beginning of the terminal line:~$
+ And install the packages:
+        pip install -r (path to requirements.txt)
+ for example(pip install -r /home/"username"/Desktop/messenger/requirements.txt)
+After that, view the list of updated packages: pip list
+ The following packages should be present there:Flask==2.3.3
+                                          Werkzeug==2.3.7
+If you have other Flask packages,         Flask-SocketIO==5.3.6
+this is normal                            gevent==24.2.1
+                                          gevent-websocket==0.10.1
+Next, log out of the directory:
+   cd
+ After successfully installing all packages (so far in Cmd), enter:
+   python3 "file path" app.py 
+ Next, just go to http://192.168.1.76:5000 /
+(close the terminal and app.py you can't)
+ To check, log in from two different devices or just open two tabs
+in the browser.
+ Unfortunately, so far the messenger only works when the page is refreshed
+and re-logged in
---
+++ app.py
@@ -0,0 +1,60 @@
+# Импорты
+from flask import Flask, render_template, request
+from flask_socketio import SocketIO
+from datetime import datetime
+from time import sleep
+
+app = Flask(__name__)
+socketio = SocketIO(app, cors_allowed_origins="*", async_mode='gevent')
+
+# Хранилище истории сообщений
+message_history = []
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+# Подключение
+@socketio.on('connect')
+def handle_connect():
+    # Логирование
+    print(f'Client connected: {request.sid}')
+
+# Отключение
+@socketio.on('disconnect')
+def handle_disconnect():
+    #Логирование
+    print(f'Client disconnected: {request.sid}')
+
+# Вход в чат
+@socketio.on('join_room')
+def handle_join_room(data):
+    username = data.get('username', 'Гость')
+    room = data.get('room', 'main_chat')
+    socketio.emit('status', {'msg': f'{username} присоединился к чату'}, room=room)
+    sleep(0.1)
+    socketio.emit('message_history', message_history, to=request.sid)
+    # Логирование
+    print(f'{username} успешно вошёл в чат {room}')
+
+# Отправка сообщений
+@socketio.on('send_message')
+def handle_send_message(data):
+    username = data['username']
+    text = data['text']
+    timestamp = datetime.now().strftime('%H:%M')
+    message = {
+        'username': username,
+        'text': text,
+        'timestamp': timestamp
+    }
+    message_history.append(message)
+    # Логирование
+    print(f'Сообщение добавлено в историю: {message}')
+    socketio.emit('new_message', message, room=data.get('room', 'main_chat'))
+    # Логирование
+    print('Отправляем new_message всем в комнате')
+    
+# Мейн
+if __name__ == '__main__':
+    socketio.run(app, host='0.0.0.0', port=5000, debug=True)
+
